29c041d7bc412aa5feb8f0bc867cfd0a224eb9e3
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-03 01:26:44 +0530

    updated as class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from "react";
 
-import './App.css';
-
-function App() {
+class App extends React.Component {
+  render(){
   return (
     <div className="App">
       
     </div>
   );
+  }
 }
 
 export default App;
