e7223763eb8db5d3685910137857f609a8882d37
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-04 01:05:28 +0530

    added agestate component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from "react";
 import './App.css';
-import {Form,FormControl,Button} from 'react-bootstrap';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import AgeStats from './components/AgeStats';
 
 class App extends React.Component {
-  render(){
-  return (
-    <div className="App">
-      <Form inline>
-        <FormControl type="date">
-        </FormControl>
-        {' '}
-        <Button>Submit</Button>
-      </Form>
-      
-    </div>
-  );
+
+  constructor() {
+    super();
+    this.state = {
+      newDate: '',
+      birthDay: '2018-09-01'
+
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      newDate: e.target.value
+    })
+
+  }
+  handleClick() {
+    console.log(this.state);
+    this.setState({
+      birthDay: this.state.newDate
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Age of students</h2>
+        <Form inline>
+          <FormControl type="date" onChange={this.handleChange}>
+          </FormControl>
+          {' '}
+
+        </Form>
+        <Button onClick={this.handleClick}>Submit</Button>
+        <AgeStats date={this.state.birthDay} />
+      </div>
+    );
   }
 }
 
---
+++ src/components/AgeStats.js
@@ -0,0 +1,11 @@
+import React from "react";
+class AgeStats extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.date}</h3>
+            </div>
+        )
+    }
+}
+export default AgeStats;
