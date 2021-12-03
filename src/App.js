0c5f9c86b4875d99f05c61e3fc7282342f3b5b38
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-03 23:43:43 +0530

    added formcontrol and submit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {Form,FormControl,Button} from 'react-bootstrap';
 
 class App extends React.Component {
   render(){
   return (
     <div className="App">
+      <Form inline>
+        <FormControl type="date">
+        </FormControl>
+        {' '}
+        <Button>Submit</Button>
+      </Form>
       
     </div>
   );
